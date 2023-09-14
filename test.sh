2122b5f4cde2b9ea3cafbf3de036e29af44d1a53
Andrew Klotz <agc.klotz@gmail.com>
2023-09-14 01:27:50 +0000

    bump goreleaser version
    dist folder name is not guaranteed to be the same between versions

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 
 set -eo pipefail
 
-trap './dist/linux_linux_amd64/reporter -debug true' EXIT
+trap './dist/linux_linux_amd64_v1/reporter -debug true' EXIT
 go test -race -count=1 -v 2>&1 ./... | go-junit-report > report.xml
