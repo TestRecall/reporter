a6d4e4e51ae1628eefd3f5e40acf65fd859e2bd2
Andrew Klotz <agc.klotz@gmail.com>
2024-04-02 21:01:15 +0000

    faster tests

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 SHELL=bash
 
 test:
-	go test -race -count=1 -v ./...
+	go test -race ./...
 
 .PHONY: setup
 setup:
--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 set -eo pipefail
 
 trap './dist/linux_linux_amd64_v1/reporter -debug true' EXIT
-go test -race -count=1 -v 2>&1 ./... | go-junit-report -iocopy -set-exit-code -out report.xml
+go test -race 2>&1 ./... | go-junit-report -iocopy -set-exit-code -out report.xml
